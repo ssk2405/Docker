993c21c1705c626530da2051bf5cf0d2b688811d
shweta <shwetakidile@gmail.com>
2025-06-18 03:33:32 +0530

    in todays section i create env variables and run it

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express= require('express');
 const app=express();
 const port=process.env.PORT || 3000;
+const appName= process.env.app_name || "Kubernetes";
 app.get('/', (req,res)=>{
-    res.send("Hello Developers its Shweta :)");
+    res.send(`Hello Developers its ${appName}`);
 });
 app.listen(port,()=>{
     console.log(`Example app listning on port ${port}`);
